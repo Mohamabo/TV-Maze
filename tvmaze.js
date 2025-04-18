a61c46268b8e67613eeffa196e373494c4cd98e2
Mohamabo <m.abouelkhair91@gmail.com>
2025-04-18 03:19:56 -0400

    Finished Code

--- tvmaze.js
+++ tvmaze.js
@@ -1,6 +1,10 @@
 "use strict";
 
+const MissingImageUrl = "https://tinyurl.com/tv-missing";
+const TVMaze_API_URL = "http://api.tvmaze.com";
+
 const $showsList = $("#showsList");
+const $episodesList = $("#episodesList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
@@ -12,29 +16,27 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+async function getShowsByTerm(term) {
+  const response = await axios({
+    baseURL : TVMaze_API_URL,
+    url: "/search/shows",
+    method: "GET",
+    params: {
+    q: term,
+  },
+ });
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ];
+  return response.data.map(result => {
+    const show = result.show;
+    return {
+      id: show.id,
+      name: show.name,
+      summary: show.summary,
+      image: show.image ? show.image.medium : MissingImageUrl
+    };
+  }
+  );
 }
-
 
 /** Given list of shows, create markup for each and to DOM */
 
@@ -46,8 +48,8 @@ function populateShows(shows) {
       `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src="${show.image}"
+              alt="${show.name} poster"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -87,8 +89,41 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+  const response = await axios({
+    baseURL: TVMaze_API_URL,
+    url: `/shows/${id}/episodes`,
+    method: "GET",
+  });
 
-/** Write a clear docstring for this function... */
+  return response.data.map(episode => ({
+    id: episode.id,
+    name: episode.name,
+    season: episode.season,
+    number: episode.number
+  }));
+}
 
-// function populateEpisodes(episodes) { }
+
+function populateEpisodes(episodes) {
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    const $item = $(
+      `<li>
+         ${episode.name} (season ${episode.season}, episode ${episode.number})
+       </li>`);
+
+    $episodesList.append($item);
+  }
+
+  $episodesArea.show();
+}
+
+async function getAndDisplayEpisodes(evt) {
+  const showId = $(evt.target).closest(".Show").data("show-id");
+  const episodes = await getEpisodesOfShow(showId);
+
+  populateEpisodes(episodes);
+}
+$showsList.on("click", ".Show-getEpisodes", getAndDisplayEpisodes);
